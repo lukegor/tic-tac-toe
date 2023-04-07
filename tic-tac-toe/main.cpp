d5a383b1310738d9d1f6bc82ed9ea1c1e64a234b
lukegor <lukaszgorski02@gmail.com>
2023-04-07 14:18:11 +0200

    add board() function

--- tic-tac-toe/main.cpp
+++ tic-tac-toe/main.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
 #include <conio.h>
+#include <cstdlib>
 
 using std::cout;
 using std::cin;
 
 void welcome();
 void enter();
+void board();
 
-char tab[3][3];
+char tab[3][3] = {{'1','2','3'},{'4','5','6'},{'7','8','9'}};
 
 int main()
 {
     welcome();
     enter();
+    system("CLS");
+
+    board();
 
     return 0;
 }
@@ -21,7 +26,7 @@ void welcome()
 {
     cout << "Welcome to our Tic-Tac-Toe game\n\n";
 
-    cout << "In order to play press ENTER...";
+    cout << "In order to start press ENTER...";
 }
 
 void enter()
@@ -31,4 +36,14 @@ void enter()
     {
         key = getch();
     }
+}
+
+void board()
+{
+    cout << "\n";
+    cout << "  " << tab[0][0] << " | " << tab[0][1] << " | " << tab[0][2] << "\n";
+    cout << " ___|___|___\n";
+    cout << "  " << tab[1][0] << " | " << tab[1][1] << " | " << tab[1][2] << "\n";
+    cout << " ___|___|___\n";
+    cout << "  " << tab[2][0] << " | " << tab[2][1] << " | " << tab[2][2] << "\n";
 }
